b69a979b1c2d444d7f4359cf786002860073905e
liuhuan <liuhuan@pudutech.com>
2018-06-28 16:35:11 +0800

    log view complit

--- logparser.py
+++ logparser.py
@@ -9,6 +9,8 @@ import re
 #     else:
 #         matchThis = "NONE"
 #     return matchThis
+levelToNum = {'VERBOSE':0, 'DEBUG':100, 'INFO':200, 'WARNING':300, 'ERROR':400, 'FATAL':500}
+
 def generateDicts(log_fh):
     currentDict = {}
     for line in log_fh:
@@ -21,17 +23,27 @@ def generateDicts(log_fh):
             i = line.find(']:')
             print('i:' + str(i))
             head = line[0:i]
-            currentDict = {"date": matched.group(1), "level": matched.group(2), "tag": matched.group(3), "file": matched.group(4), "text": matched.group(5)}
+            level = matched.group(2)
+            level_num = 0
+            if level in levelToNum:
+                level_num = levelToNum[level]
+            currentDict = {"date": matched.group(1), "level": level, 'level_num': level_num, "tag": matched.group(3), "file": matched.group(4), "text": matched.group(5)}
+            
         else:
             if("text" in currentDict):
                 currentDict["text"] += line
             else:
+                currentDict['level_num'] = 0
                 currentDict["text"] = line
     currentDict["text"] = currentDict["text"].strip()
     yield currentDict
 
-with open("log.log") as f:
-    logs= list(generateDicts(f))
+def readlog(file):
+    with open(file) as f:
+        logs= list(generateDicts(f))
+        return logs
+if __name__ == '__main__':
+    logs = readlog("log.log")
     print("log lines:", len(logs))
     for l in logs:
         print(l)
--- server.py
+++ server.py
@@ -1,11 +1,27 @@
 from flask import Flask, request, send_from_directory
 from flask_socketio import SocketIO, emit
+import logparser
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+logs = []
 
+def filter_log(level, tag, search):
+    if level == 0 and not tag and not search:
+        return logs
+    result = []
+    for l in logs:
+        if 'level_num' in l and l['level_num'] < level:
+            continue
+        if 'tag' in l and tag and l['tag'] != tag:
+            continue
+        if 'text' in l and search and l['text'].find(search) == -1:
+            continue
+        result.append(l)
+    print('filter log length:%d' % len(result))
+    return result
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
@@ -14,14 +30,24 @@ def root():
 def send_js(path):
     return send_from_directory('static', path)
 
-@socketio.on('command', namespace='/log')
-def test_message(message):
-    print('command:' + message['data'])
-    #emit('my_response', {'data': message['data']})
-
-@socketio.on('my broadcast event', namespace='/log')
-def test_message(message):
-    emit('my_response', {'data': message['data']}, broadcast=True)
+@socketio.on('log_fetch', namespace='/log')
+def log_fetch(message):
+    try:
+        print('log_fetch enter')
+        level = 0
+        tag = None
+        search = None
+        if 'level' in message:
+            level = int(message['level'])
+        if 'tag' in message:
+            tag = message['tag']
+        if 'search' in message:
+            search = message['search']
+        print('log_fetch level:%d tag:%s search:%s' % (level, tag, search))
+        emit('log_fetch', {'data': filter_log(level, tag, search)})
+    except Exception as e:
+        raise e
+    
 
 @socketio.on('connect', namespace='/log')
 def test_connect():
@@ -32,4 +58,5 @@ def test_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
+    logs = logparser.readlog("log.log")
     socketio.run(app, debug=True)
